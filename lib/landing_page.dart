61117037e75c47707e27928eb0d504da7857ac94
milordmariusz <komodo454@wp.pl>
2022-02-20 16:31:06 +0100

    update

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatelessWidget {
+  late Path ultimate;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,10 +13,39 @@ class LandingPage extends StatelessWidget {
         body: Stack(
           children: [
             ClipPath(
-              clipper: WaveClipper(),
+              clipper: WaveClipper(isShadow: true),
               child: Container(
-                color: Colors.deepOrangeAccent,
-                height: 400,
+                color: Colors.black,
+                height: (MediaQuery.of(context).size.height) -
+                    MediaQuery.of(context).size.height / 5 +
+                    50,
+              ),
+            ),
+            ClipPath(
+              clipper: WaveClipper(isShadow: true),
+              child: Container(
+                color: Colors.blue,
+                height: (MediaQuery.of(context).size.height) -
+                    MediaQuery.of(context).size.height / 5 * 2 +
+                    50,
+              ),
+            ),
+            ClipPath(
+              clipper: WaveClipper(isShadow: true),
+              child: Container(
+                color: Colors.green,
+                height: (MediaQuery.of(context).size.height) -
+                    MediaQuery.of(context).size.height / 5 * 3 +
+                    50,
+              ),
+            ),
+            ClipPath(
+              clipper: WaveClipper(isShadow: true),
+              child: Container(
+                color: Colors.yellow,
+                height: (MediaQuery.of(context).size.height) -
+                    MediaQuery.of(context).size.height / 5 * 4 +
+                    50,
               ),
             ),
           ],
@@ -26,19 +56,23 @@ class LandingPage extends StatelessWidget {
 }
 
 class WaveClipper extends CustomClipper<Path> {
+  WaveClipper({required this.isShadow});
+
+  bool isShadow;
+
   @override
   var rng = Random();
 
   Path getClip(Size size) {
     debugPrint(size.width.toString());
     var path = new Path();
-    path.lineTo(0, size.height - doubleInRange(rng, 10, 90));
-    var firstCurve = Offset(size.width / doubleInRange(rng, 3, 7),
-        size.height - doubleInRange(rng, 0, 30));
+    path.lineTo(0, size.height - doubleInRange(rng, 5, 200));
+    var firstCurve = Offset(size.width / doubleInRange(rng, 3, 8),
+        size.height - doubleInRange(rng, 0, 100));
     var secondCurve = Offset(
-        size.width - (size.width / doubleInRange(rng, 2, 4)),
-        size.height - doubleInRange(rng, 50, 150));
-    var pathEnd = Offset(size.width, size.height - doubleInRange(rng, 0, 90));
+        size.width - (size.width / doubleInRange(rng, 2, 5)),
+        size.height - doubleInRange(rng, 30, 200));
+    var pathEnd = Offset(size.width, size.height - doubleInRange(rng, 0, 230));
     path.cubicTo(firstCurve.dx, firstCurve.dy, secondCurve.dx, secondCurve.dy,
         pathEnd.dx, pathEnd.dy);
     path.lineTo(size.width, 0);
