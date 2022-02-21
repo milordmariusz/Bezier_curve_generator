c8f42dc68077e21b986521b286bc482da6a55101
milordmariusz <komodo454@wp.pl>
2022-02-21 22:23:01 +0100

    update

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const buttonColor = Color(0xFF6949FE);
+  static const textColor = Color(0xFF4C4C4C);
   static const selectedColor = Color(0xFF37E9BB);
   static const titleColor = Color(0xFF37E9BB);
   static const bgColor = Color(0xFFF3D7FF);
--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:bezier_curve_generator/app_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,8 +21,8 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Stack(
-      children: [Container(
+    return Stack(children: [
+      Container(
         padding: const EdgeInsets.fromLTRB(10, 30, 10, 10),
         height: size.height,
         width: size.width,
@@ -29,81 +30,72 @@ class _LandingPageState extends State<LandingPage> {
           color: AppColors.whiteBackground,
         ),
       ),
-        ClipShadowPath(
-          shadow: const BoxShadow(
-            color: Colors.blueGrey,
-            offset: Offset(0, 7),
-            blurRadius: 10,
-            spreadRadius: 4,
-          ),
-          clipper: SecondWaveClipperTitle(),
-          child: Container(
-            color: AppColors.whiteBackground,
-            height: (MediaQuery.of(context).size.height) -
-                MediaQuery.of(context).size.height / 5 +
-                50,
+      ClipShadowPath(
+        shadow: const BoxShadow(
+          color: AppColors.textColor,
+          offset: Offset(0, 7),
+          blurRadius: 10,
+          spreadRadius: 4,
+        ),
+        clipper: SecondWaveClipperTitle(),
+        child: Container(
+          color: AppColors.whiteBackground,
+          height: (MediaQuery.of(context).size.height) -
+              MediaQuery.of(context).size.height / 5 +
+              50,
+        ),
+      ),
+      ClipShadowPath(
+        shadow: const BoxShadow(
+          color: AppColors.textColor,
+          offset: Offset(0, 7),
+          blurRadius: 10,
+          spreadRadius: 4,
+        ),
+        clipper: FirstWaveClipperTitle(),
+        child: Container(
+          color: AppColors.whiteBackground,
+          height: (MediaQuery.of(context).size.height) -
+              MediaQuery.of(context).size.height / 5 +
+              50,
+        ),
+      ),
+      Align(
+        alignment: Alignment(-0.75, -0.60),
+        child: Text(
+          "Bezier",
+          style: GoogleFonts.raleway(
+            decoration: TextDecoration.none,
+            color: AppColors.textColor,
+            fontSize: 60,
+            //fontWeight: FontWeight.bold,
           ),
         ),
-        ClipShadowPath(
-          shadow: const BoxShadow(
-            color: Colors.blueGrey,
-            offset: Offset(0, 7),
-            blurRadius: 10,
-            spreadRadius: 4,
-          ),
-          clipper: FirstWaveClipperTitle(),
-          child: Container(
-            color: AppColors.whiteBackground,
-            height: (MediaQuery.of(context).size.height) -
-                MediaQuery.of(context).size.height / 5 +
-                50,
+      ),
+      Align(
+        alignment: Alignment(0.75, -0.30),
+        child: Text(
+          "Curve",
+          style: GoogleFonts.raleway(
+            decoration: TextDecoration.none,
+            color: AppColors.textColor,
+            fontSize: 60,
+            //fontWeight: FontWeight.bold,
           ),
         ),
-        Align(
-          alignment: Alignment(-0.75,-0.60),
-          child: NeumorphicText(
-            "Bezier",
-            style: NeumorphicStyle(
-              depth: 5,
-              color: Colors.blueGrey,
-            ),
-            textStyle: NeumorphicTextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold, //customize size here
-              // AND others usual text style properties (fontFamily, fontWeight, ...)
-            ),
+      ),
+      Align(
+        alignment: Alignment(-0.75, 0.01),
+        child: Text(
+          "Generator",
+          style: GoogleFonts.raleway(
+            decoration: TextDecoration.none,
+            color: AppColors.textColor,
+            fontSize: 60,
+            //fontWeight: FontWeight.bold,
           ),
         ),
-        Align(
-          alignment: Alignment(0.75,-0.30),
-          child: NeumorphicText(
-            "Curve",
-            style: NeumorphicStyle(
-              depth: 5,
-              color: Colors.blueGrey,
-            ),
-            textStyle: NeumorphicTextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold, //customize size here
-              // AND others usual text style properties (fontFamily, fontWeight, ...)
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment(-0.75,-0.0),
-          child: NeumorphicText(
-            "Generator",
-            style: NeumorphicStyle(
-              depth: 5,
-              color: Colors.blueGrey,
-            ),
-            textStyle: NeumorphicTextStyle(
-              fontSize: 60,
-              fontWeight: FontWeight.bold, //customize size here
-              // AND others usual text style properties (fontFamily, fontWeight, ...)
-            ),
-          ),
-        ),
+      ),
       Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -121,48 +113,44 @@ class _LandingPageState extends State<LandingPage> {
                   },
                   style: NeumorphicStyle(
                     color: AppColors.whiteBackground,
-                    shape: NeumorphicShape.flat,
+                    shape: NeumorphicShape.concave,
+                    depth: 10,
+                    intensity: 1,
                     boxShape:
-                        NeumorphicBoxShape.roundRect(BorderRadius.circular(8)),
+                        NeumorphicBoxShape.roundRect(BorderRadius.circular(50)),
                     //border: NeumorphicBorder()
                   ),
-                  padding: const EdgeInsets.all(12.0),
-                  child: Center(
-                    child: NeumorphicText(
-                      "Start",
-                      style: NeumorphicStyle(
-                          border: NeumorphicBorder(
-                            color: Color(0x33000000),
-                            width: 0.7,
-                          ),
-                          depth: 4, //customize depth here
-                          color: Color(0xFF1ECCFA) //customize color here
-                          ),
-                      textStyle: NeumorphicTextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold, //customize size here
-                        // AND others usual text style properties (fontFamily, fontWeight, ...)
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Start",
+                        style: GoogleFonts.raleway(
+                          decoration: TextDecoration.none,
+                          color: AppColors.textColor,
+                          fontSize: 60,
+                          //fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 )),
           ],
         ),
       ),
-    ]
-    );
+    ]);
   }
 }
-class FirstWaveClipperTitle extends CustomClipper<Path> {
 
+class FirstWaveClipperTitle extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     debugPrint(size.width.toString());
     var path = Path();
     path.lineTo(0, size.height / 2 - 80);
     var firstCurve = Offset(size.width / 2.5, size.height / 2 - 140);
-    var secondCurve = Offset(
-        size.width - (size.width / 2.5), size.height / 2-40);
+    var secondCurve =
+        Offset(size.width - (size.width / 2.5), size.height / 2 - 40);
     var pathEnd = Offset(size.width, size.height / 2 - 140);
     path.cubicTo(firstCurve.dx, firstCurve.dy, secondCurve.dx, secondCurve.dy,
         pathEnd.dx, pathEnd.dy);
@@ -178,15 +166,14 @@ class FirstWaveClipperTitle extends CustomClipper<Path> {
 }
 
 class SecondWaveClipperTitle extends CustomClipper<Path> {
-
   @override
   Path getClip(Size size) {
     debugPrint(size.width.toString());
     var path = Path();
-    path.lineTo(0, size.height / 2-50);
+    path.lineTo(0, size.height / 2 - 50);
     var firstCurve = Offset(size.width / 2, size.height / 2 - 50);
-    var secondCurve = Offset(
-        size.width - (size.width / 4), size.height / 2 + 50);
+    var secondCurve =
+        Offset(size.width - (size.width / 4), size.height / 2 + 50);
     var pathEnd = Offset(size.width, size.height / 2 + 40);
     path.cubicTo(firstCurve.dx, firstCurve.dy, secondCurve.dx, secondCurve.dy,
         pathEnd.dx, pathEnd.dy);
